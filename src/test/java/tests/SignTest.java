509a6b7b8fa59167e02d9f9185ab8b058d253a50
VladimirRod <vladimirrodrigues@outlook.com>
2021-09-23 20:18:09 -0300

    Initial commit

---
+++ src/test/java/tests/SignTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SignTest {
+    @Test
+    public void testFazerLoginNoTaskit(){
+        assertEquals(1, 1);
+    }
+
+}
